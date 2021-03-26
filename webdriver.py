e2d43dfa40830d9617c84024d9c556df10cf1351
Henry Herzfeld <herzfeld2@gmail.com>
2021-03-26 13:55:51 -0400

    remove debug port flag, use abspath for chromedriver

--- webdriver.py
+++ webdriver.py
@@ -18,9 +18,8 @@ class WebDriver:
         options.add_argument('--no-sandbox')
         options.add_argument('--headless')
         options.add_argument('--disable-dev-shm-usage')
-        options.add_argument("--remote-debugging-port=9222")
 
-        self.driver = webdriver.Chrome("./chromedriver", chrome_options=options)
+        self.driver = webdriver.Chrome("/var/task/chromedriver", chrome_options=options)
         self.fullscreen = fullscreen
         if self.fullscreen:
             self.driver.fullscreen_window()
