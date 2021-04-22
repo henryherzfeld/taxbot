dc4e3efccd31c946689b1097a586f6a6eed5b6f1
Henry Herzfeld <herzfeld2@gmail.com>
2021-04-22 13:48:35 -0400

    add avery, caldwell, davidson yamls

--- disable_reenable_taxbot.bat
+++ disable_reenable_taxbot.bat
@@ -3,7 +3,7 @@ echo off
 :start
 aws lambda update-event-source-mapping --uuid "9323a69d-76e0-489a-aa2f-b1dd1973072d" --enabled
 time /T
-powershell -nop -c "& {sleep 1200}"
+powershell -nop -c "& {sleep 480}"
 aws lambda update-event-source-mapping --uuid "9323a69d-76e0-489a-aa2f-b1dd1973072d" --no-enabled
 time /T
 powershell -nop -c "& {sleep 900}"
