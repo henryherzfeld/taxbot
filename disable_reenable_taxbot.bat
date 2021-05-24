1aa57eea3092ebd81824fdbdf6e2dea47481ca5a
Henry Herzfeld <herzfeld2@gmail.com>
2021-05-24 13:42:59 -0400

    add Anne Arundel (24003)

--- disable_reenable_taxbot.bat
+++ disable_reenable_taxbot.bat
@@ -3,7 +3,7 @@ echo off
 :start
 aws lambda update-event-source-mapping --uuid "9323a69d-76e0-489a-aa2f-b1dd1973072d" --enabled
 time /T
-powershell -nop -c "& {sleep 700}"
+powershell -nop -c "& {sleep 1080}"
 aws lambda update-event-source-mapping --uuid "9323a69d-76e0-489a-aa2f-b1dd1973072d" --no-enabled
 time /T
 powershell -nop -c "& {sleep 900}"
